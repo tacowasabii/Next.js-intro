73bd3ea5e43f11406ed070f324e5e65e09756b21
JUHYEON LEE <godseoril812@gmail.com>
2023-01-11 22:30:29 +0900

    styled jsx 적용

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,28 +1,29 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
-import styles from "./NavBar.module.css";
 
 export default function NavBar() {
   const router = useRouter();
   return (
     <nav>
-      <Link
-        href="/"
-        className={`${styles.link} ${
-          router.pathname === "/" ? styles.active : ""
-        }`}
-      >
-        Home
+      <Link href="/">
+        <span className={router.pathname === "/" ? "active" : ""}>Home</span>
       </Link>
-      <Link
-        href="/about"
-        className={[
-          styles.link,
-          router.pathname === "/about" ? styles.active : "",
-        ].join(" ")}
-      >
-        About
+      <Link href="/about">
+        <span className={router.pathname === "/about" ? "active" : ""}>
+          About
+        </span>
       </Link>
+      <style jsx>{`
+        nav {
+          background-color: tomato;
+        }
+        span {
+          text-decoration: none:
+        }
+        .active{
+          color: yellow;
+        }
+      `}</style>
     </nav>
   );
 }
